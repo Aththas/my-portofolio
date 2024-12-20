a78df47d608d47b13f4455976e9a7e420db5b95f
aththas <aththasrizwan123@gmail.com>
2024-12-20 10:20:11 +0530

    completed work page design

--- aththas/pages/work/index.js
+++ aththas/pages/work/index.js
@@ -1,5 +1,56 @@
+//components
+import WorkSlider from '../../components/WorkSlider';
+import Bulb from '../../components/Bulb';
+import Circles from '../../components/Circles';
+
+//framer motion
+import {motion} from 'framer-motion';
+import { fadeIn } from '../../variants';
+
 const Work = () => {
-  return <div>Work</div>;
+  return (
+    <div className='h-full bg-primary/30 py-36 flex items-center'>
+      <Circles/>
+      <div className="container mx-auto">
+        <div className='flex flex-col xl:flex-row gap-x-8'>
+          {/* text */}
+          <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0'>
+            <motion.h2 
+              className='h2 xl:mt-12'
+              variants={fadeIn('up', 0.3)}
+              initial='hidden'
+              animate='show'
+              exit='hidden'
+            >
+              My Work <span className='text-accent'>.</span>
+            </motion.h2>
+            <motion.p 
+              className='mb-4 max-w-[400px] mx-auto lg:mx-0'
+              variants={fadeIn('up', 0.2)}
+              initial='hidden'
+              animate='show'
+              exit='hidden'
+            >
+              Aspiring Software Engineer with a solid foundation in Java, C, PHP, and JavaScript. Passionate about Web and
+              Mobile Application Development, eager to contribute innovative solutions to a software team, and committed to
+              continuous learning
+            </motion.p>
+          </div>
+          {/* slider */}
+          <motion.div 
+            className='w-full xl:max-w-[65%]'
+            variants={fadeIn('down', 0.6)}
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+          >
+            <WorkSlider/>
+          </motion.div>
+        </div>
+      </div>
+      <Bulb/>
+    </div>
+  );
 };
 
 export default Work;
